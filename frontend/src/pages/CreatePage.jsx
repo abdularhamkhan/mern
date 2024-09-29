6c8645391fb087b7c6e109b22647c531c9898247
abdularhamkhanzada <abdularhamkhan02@gmail.com>
2024-09-29 19:29:55 +0500

    added UI for createPage

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,25 +1,59 @@
 import React, { useState } from 'react'
-import { Container } from '@chakra-ui/react'
-import { VStack } from '@chakra-ui/react'
-import { Heading } from '@chakra-ui/react'
+import { Container, VStack, Heading, useColorModeValue, Box, Input, Button} from '@chakra-ui/react'
+
 
 const CreatePage = () => {
-  const [newProduct, setProduct] = useState({
+  const [newProduct, setNewProduct] = useState({
     name: "",
     price: "",
     image: "",
   })
+
+  const handleAddProduct =()=>{
+    console.log(newProduct)
+  }
   return (
     <Container maxW={"container.sm"}>
       <VStack
         spacing={8}
       >
-        <Heading>
+        <Heading as={"h1"} size={"2xl"} textAlign={"center"} mb={8}>
 
           Create New Product
         </Heading>
+
+        <Box
+          w={"full"} bg={useColorModeValue("white", "gray.800")} p={6} rounded={"lg"}
+          shadow={"md"}
+        >
+          <VStack spacing={8}>
+            <Input
+              placeholder='Product Name'
+              name='name'
+              value={newProduct.name}
+              onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            />
+            <Input
+              placeholder='Price'
+              name='price'
+              value={newProduct.price}
+              type="number"
+              onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}
+            />
+            <Input
+              placeholder='Image URL'
+              name='image'
+              value={newProduct.image}
+              onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}
+            />
+
+            <Button colorScheme="blue" onClick={handleAddProduct} w='full'>
+              Add Product
+            </Button>
+          </VStack>
+        </Box>
       </VStack>
-      Create your products here
+
     </Container>
   )
 }
